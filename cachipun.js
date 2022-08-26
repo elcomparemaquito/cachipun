a387ce075838d9da49daef137ee7c79e6532e5b3
Maximiliano Cifuentes <maxcifuentesads@gmail.com>
2022-08-25 21:00:35 -0400

    version final

--- cachipun.js
+++ cachipun.js
@@ -1,29 +1,32 @@
 function getComputerChoice(){
     let opcionesCachipun = ["rock","paper","scissors"];
-    return opcionesCachipun[Math.floor(Math.random() * 3)];
+    let computerChoice = opcionesCachipun[Math.floor(Math.random() * 3)];
+    return computerChoice;
 }
+
+function getPlayerChoice(){
+    let selection = prompt("rock, paper or scissors=");
+    let playerChoice = selection.toLowerCase()
+    return playerChoice
+}
+
 
 function playRound(playerSelection, computerSelection) {
     if (playerSelection == computerSelection){
         return "You Tie!"
     }
-    let solutions = {rockscissors: "You Win! Rock beats Scissors", rockpaper: "You Lose! Paper beats Rock!", scissorsrock: "You Lose! Rock beats Scissors", scissorspaper: "You Win! Scissors beats Paper", paperrock: "You Win! Paper beats Rock", paperscissors: "You Lose! Scissors beats Paper"};
-    let result = playerSelection +computerSelection
+    let solutions = {"rockscissors": "You Win! Rock beats Scissors", "rockpaper": "You Lose! Paper beats Rock!", "scissorsrock": "You Lose! Rock beats Scissors", "scissorspaper": "You Win! Scissors beats Paper", "paperrock": "You Win! Paper beats Rock", "paperscissors": "You Lose! Scissors beats Paper"};
+    let result = playerSelection + computerSelection
     return solutions[result]
   }
 
-function playerSelection(){
-    let selection = prompt("rock, paper or scissors=");
-    return selection.toLowerCase()
-}
-const computerSelection = getComputerChoice();
-
 function game(){
     for (let i = 0; i < 5; i++) {
-        let jugador = playerSelection()
-        let computador = getComputerChoice()
-        let resultado = playRound(jugador,computador)
+        const computerSelection = getComputerChoice();
+        const playerSelection = getPlayerChoice();
+        let resultado = playRound(playerSelection,computerSelection)
         console.log(resultado)
     }
 }
 
+game()
